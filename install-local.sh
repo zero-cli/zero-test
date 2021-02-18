a4f8daf30f49bacb481da4f86006f2550923c7f3
Brent Mealhouse <bmealhouse@gmail.com>
2021-02-18 14:39:30 -0600

    Update examples

---
+++ install-local.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+rm zero-cli-zero-test-*.*.*.tgz
+npm pack
+TGZ=../../$(ls zero-cli-zero-test-*.*.*.tgz)
+
+for examplePath in examples/*/ ; do
+  pushd $examplePath
+  yarn add file:$TGZ --dev
+  popd
+done
